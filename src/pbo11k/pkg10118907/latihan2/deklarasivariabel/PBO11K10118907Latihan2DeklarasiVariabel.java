ade6e9bd57b0ca4e6d57bd6999ac4e1821a85882
unknown <fadlullahyazid@gmail.com>
2019-09-24 23:09:59 +0700

    First commit

---
+++ src/pbo11k/pkg10118907/latihan2/deklarasivariabel/PBO11K10118907Latihan2DeklarasiVariabel.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo11k.pkg10118907.latihan2.deklarasivariabel;
+
+/**
+ *
+ * @author
+ * NAMA     : Yazid Fadlullah At-Taukily
+ * KELAS    : PBO11K
+ * NIM      : 10118907
+ * Deskripsi Program : Latihan mendeklarasikan variabel
+ * 
+ */
+public class PBO11K10118907Latihan2DeklarasiVariabel {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        int nilaiInt;
+        final double PHI = 3.14;
+        boolean nilaiLogika;
+        char nilaiKarakter;
+
+        nilaiInt = 78;
+        nilaiLogika = false;
+        nilaiKarakter = 'D';
+
+        System.out.println();
+        System.out.println("Isi variabel nilai =" +nilaiInt);
+        System.out.println("Isi variabel PHI =" + PHI);
+        System.out.println("Isi variabel Logika =" + nilaiLogika);
+        System.out.println("Isi variabel Karakter =" + nilaiKarakter);
+        System.out.println("Isi variabel ");
+    }
+    
+}
